d372e6ecc6e4cade812bd9ff27a2fc213feb6b33
Tushar Batra <tusharbatra08@gmail.com>
2024-12-14 17:34:12 +0530

    Handled Functionality for my small Todo-App.

---
+++ Todo-List/script.js
@@ -0,0 +1,31 @@
+let username = document.getElementById("Username");
+let password = document.getElementById("password");
+
+let login = false;
+
+function handleLogin() {
+  if (username.value === "tushar" && password.value === "Tushar@123") {
+    document.getElementById("message").innerHTML = "Login Successful";
+    login = true;
+    console.log(login);
+
+    if (login) {
+      window.location.href = "list.html";
+    }
+  } else {
+    document.getElementById("message").innerHTML = "Login Failed";
+  }
+}
+
+let todos = [];
+
+function handleAddTodo() {
+  todos.push(document.getElementById("Todo"));
+
+  const newTodoInput = document.getElementById("newTodo");
+
+  const li = document.createElement("li");
+  li.textContent = newTodoInput.value;
+  document.getElementById("todo-list").appendChild(li);
+  console.log(todos);
+}
