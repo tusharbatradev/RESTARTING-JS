393ead6c13d511a3b2db5c69d02ba1674fc4f4ac
Tushar Batra <tusharbatra08@gmail.com>
2024-12-18 10:32:02 +0530

    Handled Backend with http server also understand Routing.

--- Backend/app.js
+++ Backend/app.js
@@ -1,3 +1,13 @@
-const catMe = require('cat-me');
+const http = require("http");
 
-console.log(catMe());
+const server = http.createServer((req, res) => {
+  if (req.url == "/about") {
+    res.end("This is About Page");
+  } else if (req.url == "/profile") {
+    res.end("This is a Profile Page");
+  } else if (req.url == "/") {
+    res.end("Hello World!");
+  }
+});
+
+server.listen(3000);
