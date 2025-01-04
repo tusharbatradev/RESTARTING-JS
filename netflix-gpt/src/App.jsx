6ce91729f46d84680f9a240cc235bda0be89acc8
Tushar Batra <tusharbatra08@gmail.com>
2025-01-04 11:01:35 +0530

    NetflixGpt CRA & Tailwind

---
+++ netflix-gpt/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="opacity-0 pointer-events-none">
+      <h1 className="text-red-200">Invisible but styled</h1>
+    </div>
+  );
+}
+
+export default App;
